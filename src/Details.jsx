2dc023f2b0765d5b0a3a0226aab3efb3e6780341
NyashaMuusha <nyashamuusha@gmail.com>
2023-10-08 20:38:34 +0200

    added product details to component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 import PageNotFound from "./PageNotFound";
 
-export default function Detail() {
+export default function Detail({ addToCart }) {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [sku, setSku] = useState("");
+
   const { data: product, loading, error } = useFetch(`products/${id}`);
 
   if (loading) return <Spinner />;
@@ -18,8 +20,23 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <select id="size" value={sku} onChange={(e) => setSku(e.target.value)}>
+        <option value="">What size? </option>
+        {product.skus.map((s) => (
+          <option value={s.sku} key={s.sku}>
+            {s.size}
+          </option>
+        ))}
+      </select>
       <p>
-        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+        <button
+          className="btn btn-primary"
+          disabled={!sku}
+          onClick={() => {
+            navigate("/cart");
+            addToCart(id, sku);
+          }}
+        >
           Add to cart
         </button>
       </p>
