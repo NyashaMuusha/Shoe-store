cd71c086d4b46f44c73eb968b7316bddccf37b2b
NyashaMuusha <nyashamuusha@gmail.com>
2023-10-09 09:41:22 +0200

    small fixes

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,29 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const baseUrl = "http://localhost:3001/";
 
 export default function useFetch(url) {
+  const isMounted = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    isMounted.current = true;
     async function init() {
       try {
         const response = await fetch(baseUrl + url);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+          if (isMounted.current) setData(json);
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e);
+        if (isMounted.current) setError(e);
       } finally {
         setLoading(false);
       }
     }
     init();
+
+    return () => isMounted.current = false;
   }, [url]);
 
   return { data, error, loading };
--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,12 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { baseUrl } from "./useFetch";
 
 export default function useFetchAll(urls) {
+  const prevUrls = useRef([]);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    //Only run is the array of urls passed in changes
+    if (areEqual(prevUrls.current, urls)) {
+      setLoading(false);
+      return
+    };
+    
+    prevUrls.current = urls;
     const promises = urls.map((url) =>
       fetch(baseUrl + url).then((response) => {
         if (response.ok) return response.json();
@@ -25,4 +33,10 @@ export default function useFetchAll(urls) {
   }, []);
 
   return { data, loading, error };
+}
+
+const areEqual = (array1, array2) => {
+  return (
+    array1.length === array2.length && array1.every((value, index) => value === array2(index))
+  )
 }
