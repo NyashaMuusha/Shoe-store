ee43bb841c8850f397bb4fa85169e02153b8ddc9
NyashaMuusha <nyashamuusha@gmail.com>
2023-10-09 02:26:29 +0200

    added shipping service

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import useFetchAll from "./services/useFetchAll";
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
 
 export default function Cart({ cart, updateQuantity }) {
+  const navigate = useNavigate();
   const urls = cart.map((i) => `products/${i.id}`);
   const { data: products, loading, error } = useFetchAll(urls);
 
   function renderItem(itemInCart) {
     const { id, sku, quantity } = itemInCart;
     const { price, name, image, skus } = products.find(
-      (p) => p.id === parseInt(id)
+      (p) => p.id === parseInt(id),
     );
     const { size } = skus.find((s) => s.sku === sku);
 
@@ -52,6 +54,14 @@ export default function Cart({ cart, updateQuantity }) {
           : `${numItemsInCart} Item${numItemsInCart > 1 ? "s" : ""} in My Cart`}
       </h1>
       <ul>{cart.map(renderItem)}</ul>
+      {cart.length > 0 && (
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/checkout")}
+        >
+          Checkout
+        </button>
+      )}
     </section>
   );
 }
--- src/services/shippingService.js
+++ src/services/shippingService.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.REACT_APP_API_BASE_URL;
+import { baseUrl } from "./useFetch";
 
 export async function getShippingAddress(userId) {
   return fetch(baseUrl + "shippingAddress/" + userId).then((response) => {
